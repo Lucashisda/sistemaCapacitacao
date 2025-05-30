833c3caa1f102ced4196faf6ab235048a950b569
Lucas wi <bezerra.l@estudante.ifmt.edu.br>
2025-05-30 11:02:33 -0400

    c\zxc

--- sistemaCapacitacao-main/src/aplicacao/Programa.java
+++ sistemaCapacitacao-main/src/aplicacao/Programa.java
@@ -1,17 +1,30 @@
 package aplicacao;
 
-import entidades.ServidorPublico;
+import entidade.ServidorPublico;
 
 public class Programa {
-    public static void main(String[] args) {
+    public static void main(String [] args ){
         ServidorPublico isabela = new ServidorPublico();
-        isabela.setNome("Isabela");
+        isabela.setNome("isabela");
         isabela.setCargo("Auditor");
         isabela.setOrgao("ANVISA");
         isabela.setLotacao("Brasilia");
-        isabela.setEmail("isabela@gmail.");
+        isabela.setEmail("isabela@gmail.com");
+        isabela.setNome("João");
+        isabela.calcularSalariohorasExtras(10, 5.60);
 
-        System.out.println("Servidor: "+ isabela.getNome());
+
+        System.out.println("Servidor: " + isabela.getNome());
+        System.out.printf("Horas extras R$ % 2.f : "+isabela, ServidorPublico.getHorasExtras());
+        isabela.calcularNumeros(4,6,28,56);
+
+
+
+
+
+
+
+
+
 
     }
-}
--- sistemaCapacitacao-main/src/entidades/ServidorPublico.java
+++ sistemaCapacitacao-main/src/entidades/ServidorPublico.java
@@ -1,130 +1,215 @@
-package entidades;
+package entidade;
+
+import javax.sound.midi.Soundbank;
 
 public class ServidorPublico {
-        private int matricula;
-        private String nome;
-        private String foto;
-        private String orgao;
-        private String vinculo;
-        private double salario;
-        private int idade;
-        private int tempoDeContribuicao;
-        private String cargo;
-        private String telefone;
-        private String celular;
-        private String cpf;
-        private String lotacao;
-        private String email;
 
-        public String getEmail() {
-                return email;
+    private int material;
+    private String orgao;
+    private String foto;
+    private String vinculo;
+    private double salario;
+    private int idade;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public static Object getHorasExtras() {
+        return null;
+    }
+
+    private int tempoDeContribuicao;
+    private String email;
+    private double horasExtras;
+
+    public double HorasExtras() {
+        return horasExtras;
+    }
+
+    public void setHorasExtras(double horasExtras) {
+        this.horasExtras = horasExtras;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLotacao() {
+        return lotacao;
+    }
+
+    public void setLotacao(String lotacao) {
+        this.lotacao = lotacao;
+    }
+
+    private String cargo;
+    private String telefone;
+    private String celular;
+    private String cpf;
+    private String naturalidade;
+
+    private String lotacao;
+
+    ServidorPublico isabela = new ServidorPublico();
+    ServidorPublico heila = new ServidorPublico();
+    ServidorPublico maria = new ServidorPublico();
+    ServidorPublico caio = new ServidorPublico();
+
+    public int getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(int material) {
+        this.material = material;
+    }
+
+    public String getOrgao() {
+        return orgao;
+    }
+
+    public void setOrgao(String orgao) {
+        this.orgao = orgao;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getVinculo() {
+        return vinculo;
+    }
+
+    public void setVinculo(String vinculo) {
+        this.vinculo = vinculo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getTempoDeContribuicao() {
+        return tempoDeContribuicao;
+    }
+
+    public void setTempoDeContribuicao(int tempoDeContribuicao) {
+        this.tempoDeContribuicao = tempoDeContribuicao;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNaturalidade() {
+        return naturalidade;
+    }
+
+    public void setNaturalidade(String naturalidade) {
+        this.naturalidade = naturalidade;
+    }
+
+    public ServidorPublico getIsabela() {
+        return isabela;
+    }
+
+    public void setIsabela(ServidorPublico isabela) {
+        this.isabela = isabela;
+    }
+
+    public ServidorPublico getHeila() {
+        return heila;
+    }
+
+    public void setHeila(ServidorPublico heila) {
+        this.heila = heila;
+    }
+
+    public ServidorPublico getMaria() {
+        return maria;
+    }
+
+    public void setMaria(ServidorPublico maria) {
+        this.maria = maria;
+    }
+
+    public ServidorPublico getCaio() {
+        return caio;
+    }
+
+    public void setCaio(ServidorPublico caio) {
+        this.caio = caio;
+    }
+
+    public double calcularSalariohorasExtras(double horastrabalhadas, double valorHora) {
+        double salarioMensal = salario +
+                (horastrabalhadas * valorHora);
+        horasExtras = salarioMensal;
+        return (salarioMensal);
+    }
+    double soma = 0;
+
+    public double calcularNumeros(double... numeros) {
+        for (double numero : numeros) {
+            soma += numero;
         }
 
-        public void setEmail(String email) {
-                this.email = email;
-        }
+        System.out.println("A soma dos argumentos informados"+ soma);
+        return soma;
 
-        public String getLotacao() {
-                return lotacao;
-        }
+    }
 
-        public void setLotacao(String lotacao) {
-                this.lotacao = lotacao;
-        }
 
-        public int getMatricula() {
-                return matricula;
-        }
-
-        public void setMatricula(int matricula) {
-                this.matricula = matricula;
-        }
-
-        public String getNome() {
-                return nome;
-        }
-
-        public void setNome(String nome) {
-                this.nome = nome;
-        }
-
-        public String getFoto() {
-                return foto;
-        }
-
-        public void setFoto(String foto) {
-                this.foto = foto;
-        }
-
-        public String getOrgao() {
-                return orgao;
-        }
-
-        public void setOrgao(String orgao) {
-                this.orgao = orgao;
-        }
-
-        public String getVinculo() {
-                return vinculo;
-        }
-
-        public void setVinculo(String vinculo) {
-                this.vinculo = vinculo;
-        }
-
-        public double getSalario() {
-                return salario;
-        }
-
-        public void setSalario(double salario) {
-                this.salario = salario;
-        }
-
-        public int getIdade() {
-                return idade;
-        }
-
-        public void setIdade(int idade) {
-                this.idade = idade;
-        }
-
-        public int getTempoDeContribuicao() {
-                return tempoDeContribuicao;
-        }
-
-        public void setTempoDeContribuicao(int tempoDeContribuicao) {
-                this.tempoDeContribuicao = tempoDeContribuicao;
-        }
-
-        public String getCargo() {
-                return cargo;
-        }
-
-        public void setCargo(String cargo) {
-                this.cargo = cargo;
-        }
-
-        public String getTelefone() {
-                return telefone;
-        }
-
-        public void setTelefone(String telefone) {
-                this.telefone = telefone;
-        }
-
-        public String getCelular() {
-                return celular;
-        }
-
-        public void setCelular(String celular) {
-                this.celular = celular;
-        }
-
-        public String getCpf() {
-                return cpf;
-        }
-
-        public void setCpf(String cpf) {
-                this.cpf = cpf;
-        }
 }
